af9384148955887c0ef5def2ac2352772c8a59af
Caroline DebBaruah <cdebbaruah@gmail.com>
2022-11-28 02:47:26 -0800

    add hero and hero image

---
+++ client/src/types/home.ts
@@ -0,0 +1,14 @@
+export interface mainHero {
+    title: string;
+    subtitle: string;
+    description: string;
+    img: string;
+    primaryAction: {
+      text: string;
+      href: string;
+    },
+}
+
+export interface heroImage {
+    
+}
