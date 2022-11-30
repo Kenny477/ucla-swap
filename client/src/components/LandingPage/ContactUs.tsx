b0cc41ee7da81914a472c64ad3396ab9ef22f8af
Gayathri Eleswarapu <gayathrieles16@PersonalMicrosoftSoftware.ucla.edu>
2022-11-30 15:30:37 -0800

    Each member's name takes you to their github

---
+++ client/src/components/LandingPage/ContactUs.tsx
@@ -0,0 +1,17 @@
+function Contact()
+{
+    return (
+        <div>
+            <h1>
+            Contact Us:
+            </h1>
+            <a href={"https://www.linkedin.com/in/alan-michael-64383b19a/"}>Alan Michael</a>
+            <a href={"https://www.linkedin.com/in/caroline-debbaruah/"}>Caroline DebBaruah</a>
+            <a href={"https://www.linkedin.com/in/gayathri-eleswarapu-1b3a97190/"}>Gayathri Eleswarapu</a>
+            <a href={"https://www.linkedin.com/in/kennethtang0/"}>Kenny Tran</a>
+            <a href={"https://www.linkedin.com/in/liammeagher03/"}>Liam Meagher</a>
+            
+        </div>
+    )
+}
+export default Contact;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import Hero from "../components/LandingPage/Hero";
 import Features from "../components/LandingPage/Features";
+import Contact from "../components/LandingPage/ContactUs";
 
 function Home() {
-	return (
-		<div className="flex flex-col">
-			<Hero />
-			<Features />
-		</div>
-	);
+ return (
+  <div className="flex flex-col">
+	<Hero />
+	<Features />
+	<Contact/>
+  </div>
+ );
 };
 
 export default Home;
