8cf12ca5b9fbdb2da68883f4694fcde0c580e2b5
Gayathri Eleswarapu <gayathrieles16@PersonalMicrosoftSoftware.ucla.edu>
2022-11-30 17:09:19 -0800

    Fixed Kenny's name pt 2

--- client/src/components/LandingPage/ContactUs.tsx
+++ client/src/components/LandingPage/ContactUs.tsx
@@ -7,7 +7,7 @@ function Contact()
                 <a href={"https://www.linkedin.com/in/alan-michael-64383b19a/"}>Alan Michael</a>
                 <a href={"https://www.linkedin.com/in/caroline-debbaruah/"}>Caroline DebBaruah</a>
                 <a href={"https://www.linkedin.com/in/gayathri-eleswarapu-1b3a97190/"}>Gayathri Eleswarapu</a>
-                <a href={"https://www.linkedin.com/in/kennethtang0/"}>Kenny Tang</a>
+                <a href={"https://www.linkedin.com/in/kennethtang0/"}>Kenneth Tang</a>
                 <a href={"https://www.linkedin.com/in/liammeagher03/"}>Liam Meagher</a>
             </div>
         </section>
