5edee114a098f4ace85a6450a6ba36f355fb947a
Gayathri Eleswarapu <gayathrieles16@PersonalMicrosoftSoftware.ucla.edu>
2022-12-01 13:32:03 -0800

    Names are spaced and alligned horizonatally

--- client/src/components/LandingPage/ContactUs.tsx
+++ client/src/components/LandingPage/ContactUs.tsx
@@ -5,6 +5,12 @@ const divStyle =
     padding: 50
 };
 
+const spacer = 
+{
+    height: '25px',
+    width: '25px',
+}
+
 function Contact()
 {
     return (
@@ -12,10 +18,15 @@ function Contact()
             <div className="flex flex-col items-center justify-center h-full space-y-8">
                 <div style={divStyle}>
                     <h1 className="text-2xl font-bold">Contact Us:</h1>
+                    <div style = {spacer}/>
                     <a href={"https://www.linkedin.com/in/alan-michael-64383b19a/"}>Alan Michael</a>
+                    <div style = {spacer}/>
                     <a href={"https://www.linkedin.com/in/caroline-debbaruah/"}>Caroline DebBaruah</a>
+                    <div style = {spacer}/>
                     <a href={"https://www.linkedin.com/in/gayathri-eleswarapu-1b3a97190/"}>Gayathri Eleswarapu</a>
+                    <div style = {spacer}/>
                     <a href={"https://www.linkedin.com/in/kennethtang0/"}>Kenneth Tang</a>
+                    <div style = {spacer}/>
                     <a href={"https://www.linkedin.com/in/liammeagher03/"}>Liam Meagher</a>
                 </div>
             </div>
