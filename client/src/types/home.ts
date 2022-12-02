96b462f4d38bd0821a3cdc7ee562944d4e74da78
Alan Michael <alan.michael1018@gmail.com>
2022-12-01 18:13:42 -0800

    Added About Us Page

--- client/src/types/home.ts
+++ client/src/types/home.ts
@@ -30,3 +30,37 @@ export interface features {
         img: string,
       },
 }
+
+export interface features2 {
+  title: string,
+  item1:
+    {
+      title: string,
+      description: string,
+      img: string,
+    },
+    item2:
+    {
+      title: string,
+      description: string,
+      img: string,
+    },
+    item3:
+    {
+      title: string,
+      description: string,
+      img: string,
+    },
+    item4:
+    {
+      title: string,
+      description: string,
+      img: string,
+    },
+    item5:
+    {
+      title: string,
+      description: string,
+      img: string,
+    },
+}
